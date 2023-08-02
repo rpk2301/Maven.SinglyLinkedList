688ce19c0f423b94b4a40cc50a43cee9509b8b5d
Ricky Kyte <137026079+rpk2301@users.noreply.github.com>
2023-08-02 10:29:57 -0400

    added some stubs

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,115 +1,49 @@
 package com.zipcodewilmington.singlylinkedlist;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList   {
-    Node head;
-    public SinglyLinkedList()
-    {
-    head = new Node();
-    }
-    public static class Node
-    {
-        int data;
-        Node next;
+public class SinglyLinkedList<T> implements  LinkedListIface{
 
-        public Node()
-        {
-            data = 0;
-            next = null;
-        }
-        public Node(Node node)
-        {
-            this.data =0;
-            this.next = node;
-        }
-        public Node getNext() {
-            return next;
-        }
-        public void setNext(Node next)
-        {
-            this.next = next;
-        }
-    }
-
-   public Node getTail(Node head)
-   {
-       if(hasNext(head))
-       {
-           return getTail(head.next);
-       }
-       return head;
-   }
-
-   public boolean hasNext(Node node)
-   {
-       if(node.getNext()!=null)
-       {
-           return true;
-       }
-       else return false;
-   }
-
-   public Node gotoNext(Node n)
-   {
-       return n.getNext();
-   }
-
-    public Node add(Node n)
-    {
-     getTail(head).setNext(n);
-     return n;
-    }
-
-    public void remove(int index)
-    {
-       Node p = get(index);
-       Node f = get(index+1);
-       Node r = get(index-1);
-       r.setNext(f);
-       p.setNext(null);
-
+    @Override
+    public void add(Object data) {
 
     }
 
-    public Node get(int ret)
-    {
-        if(ret==0)
-        {
-            return this.head;
-        }
-        else
-        {
-            Node p = head;
-            for(int i=1;i<=ret;i++)
-            {
-                p = p.getNext();
-            }
-            return p;
-        }
+    @Override
+    public void remove(int index) throws IndexOutOfBoundsException {
 
     }
 
-
-    public int size()
-    {
-        int count=2;
-        if(!hasNext(head))
-        {
-            return 1;
-        }
-        Node p =head.getNext();
-        {
-            while(p.getNext()!=null)
-            {
-                p = p.getNext();
-                count++;
-            }
+    @Override
+    public Object get(int index) throws IndexOutOfBoundsException {
+        return null;
     }
-return count;
+
+    @Override
+    public boolean contains(Object data) {
+        return false;
+    }
+
+    @Override
+    public int find(Object data) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public SinglyLinkedList copy() {
+        return null;
+    }
+
+    @Override
+    public void sort(Comparator comparator) {
+
     }
 }
