1fdaa00e32c797f4ea04a715165dbae91be4608b
Ricky Kyte <137026079+rpk2301@users.noreply.github.com>
2023-08-02 11:31:41 -0400

    Passed some tests

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -7,9 +7,28 @@ import java.util.Comparator;
  */
 public class SinglyLinkedList<T> implements  LinkedListIface{
 
+    private Node<T> head = null;
+
+
+    public SinglyLinkedList()
+    {
+
+    }
+
     @Override
     public void add(Object data) {
-
+    Node newnode = new Node(data);
+    if(this.head == null)
+    {
+        this.head = newnode;
+    }
+    else{
+        Node<T> temp = head;
+        while(temp.hasNext()){
+            temp = temp.getNext();
+        }
+        temp.setNext(newnode);
+    }
     }
 
     @Override
@@ -46,4 +65,35 @@ public class SinglyLinkedList<T> implements  LinkedListIface{
     public void sort(Comparator comparator) {
 
     }
-}
+
+    public Boolean isEmpty()
+    {
+        if(head == null)
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("head -> ");
+        Node<T> temp = head;
+        if(head!=null) {
+            do {
+
+                sb.append("[" + temp.getData() + "]" + " -> ");
+                temp = temp.getNext();
+            }
+            while((temp != null));// && temp.hasNext());
+
+        }
+
+        sb.append("NULL");
+            return sb.toString();
+
+
+
+}}
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -8,8 +8,69 @@ import org.junit.*;
 public class SinglyLinkedListTest {
 
 
+@Test
+    public void testConstructor()
+{
+    SinglyLinkedList<Integer> llist = new SinglyLinkedList<>();
+    Assert.assertNotNull(llist);
+}
 
 
+@Test
+    public void testIsEmpty()
+{
+    SinglyLinkedList<Integer> llist = new SinglyLinkedList<Integer>();
+    Assert.assertEquals((Boolean) true, llist.isEmpty());
+}
+
+
+    @Test
+    public void testAdd1()
+    {
+        SinglyLinkedList<Integer> llist = new SinglyLinkedList<Integer>();
+        llist.add(1);
+        Assert.assertEquals((Boolean) false, llist.isEmpty());
+    }
+
+    @Test
+    public void testAdd2()
+    {
+        SinglyLinkedList<Integer> llist = new SinglyLinkedList<Integer>();
+        llist.add(1);
+        llist.add(2);
+        Assert.assertEquals((Boolean) false, llist.isEmpty());
+    }
+
+
+    @Test
+    public void testToString()
+    {
+        SinglyLinkedList<Integer> llist = new SinglyLinkedList<>();
+        String expected = "head -> NULL";
+        String actual = llist.toString();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testToString2()
+    {
+        SinglyLinkedList<Integer> llist = new SinglyLinkedList<>();
+        llist.add(1);
+        String expected = "head -> [1] -> NULL";
+        String actual = llist.toString();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testToString3()
+    {
+        SinglyLinkedList<Integer> llist = new SinglyLinkedList<>();
+        llist.add(1);
+        llist.add(2);
+        String expected = "head -> [1] -> [2] -> NULL";
+        String actual = llist.toString();
+        Assert.assertEquals(expected,actual);
+    }
 
 }
 
